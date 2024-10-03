c227ef7c4798c47fbd594ae4e79516b755fe4dca
Salih Mohammed <Salih-Mohammed@gmail.com>
2024-10-03 11:19:50 +0300

    Add css

---
+++ app/ui/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./home.module.css";
+function Navbar() {
+  return (
+    <div className="flex min-h-screen flex-col p-6">
+      {" "}
+      <div className={styles.shape} />
+    </div>
+  );
+}
+
+export default Navbar;
